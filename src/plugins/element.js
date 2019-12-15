8ad02bc2c796c74b8d60faf86e230675429a0376
juedui0769 <chixin0769@163.com>
2019-12-15 22:09:32 +0800

    添加 button 样例

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
-import Element from 'element-ui'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import '../element-variables.scss'
 
-Vue.use(Element)
+Vue.use(ElementUI)
